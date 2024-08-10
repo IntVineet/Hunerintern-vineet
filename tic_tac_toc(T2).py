cd87525c41b556cf65bc44a320c6f5c75e57371c
Vineet Kumar <vineetworks07@gmail.com>
2024-08-09 22:38:55 -0700

    final commit

--- tic_tac_toc(T2).py
+++ tic_tac_toc(T2).py
@@ -3,7 +3,7 @@ def print_board():
     row1 = "| {} | {} | {} |".format(board[0], board[1], board[2])
     row2 = "| {} | {} | {} |".format(board[3], board[4], board[5])
     row3 = "| {} | {} | {} |".format(board[6], board[7], board[8])
-
+ 
 
     print()
     print(row1)
